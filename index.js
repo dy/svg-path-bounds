95a6926eeb12288f8f91bc408b5d3a2adecbbe39
Dmitry <df.creative@gmail.com>
2017-08-24 19:35:16 +0300

    ES5ify

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 'use strict'
 
-const parse = require('parse-svg-path')
-const abs = require('abs-svg-path')
-const normalize = require('normalize-svg-path')
-const isSvgPath = require('is-svg-path')
-const assert = require('assert')
+var parse = require('parse-svg-path')
+var abs = require('abs-svg-path')
+var normalize = require('normalize-svg-path')
+var isSvgPath = require('is-svg-path')
+var assert = require('assert')
 
 module.exports = pathBounds
 
@@ -26,12 +26,12 @@ function pathBounds(path) {
 
   if (!path.length) return [0, 0, 0, 0]
 
-  let bounds = [Infinity, Infinity, -Infinity, -Infinity]
+  var bounds = [Infinity, Infinity, -Infinity, -Infinity]
 
-  for (let i = 0, l = path.length; i < l; i++) {
-    let points = path[i].slice(1)
+  for (var i = 0, l = path.length; i < l; i++) {
+    var points = path[i].slice(1)
 
-    for (let j = 0; j < points.length; j += 2) {
+    for (var j = 0; j < points.length; j += 2) {
       if (points[j + 0] < bounds[0]) bounds[0] = points[j + 0]
       if (points[j + 1] < bounds[1]) bounds[1] = points[j + 1]
       if (points[j + 0] > bounds[2]) bounds[2] = points[j + 0]
